0f861741aee44592866da4ba05372765ca2b035e
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-21 19:59:01 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import './App.css';
 import { Title } from "./Title.jsx";
 
-function Description() {
-  return <h3>I am the Description!</h3>;
-}
-
 function App() {
    return (
     <div>
       <Title />
-      <Description />
       <Title />
-      <Description />
     </div>
    );
 }
 
-export default App
+export default App;
