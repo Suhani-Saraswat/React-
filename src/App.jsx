2cd6617717f233329bc5a80e2406f9d97f7cf07c
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-22 23:20:05 +0530

    Amazon Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css';
-import { Title } from "./Title.jsx";
 import ProductTab from './ProductTab.jsx';
-import MsgBox from './MsgBox.jsx';
 
 function App() {
    return (
    <>
-      <MsgBox userName="Suhani" textColor="pink"/>
-      <MsgBox userName="apnaCollege" textColor="blue"/>
+      <h2 style={{textAlign: "center"}}>Blockbuster Deals | Shop Now</h2>
       <ProductTab />
    </>
    );
