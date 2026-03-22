f692c6aef5d33929f0c19cade3bcc4431cc0c30e
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-22 21:23:29 +0530

    Activity

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
 import { Title } from "./Title.jsx";
 import ProductTab from './ProductTab.jsx';
+import MsgBox from './MsgBox.jsx';
 
 function App() {
    return (
-    <ProductTab />
+   <>
+      <MsgBox userName="Suhani" textColor="pink"/>
+      <MsgBox userName="apnaCollege" textColor="blue"/>
+      <ProductTab />
+   </>
    );
 }
 
