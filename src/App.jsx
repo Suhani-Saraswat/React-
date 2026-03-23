03e393e52df73bc0f21f1f9ad796e062130ae081
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-23 21:08:55 +0530

    Form Basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import { Button } from './Button.jsx';
+import Form from "./Form.jsx";
 
 function App() {
    return (
    <>
-      <Button /> 
+      <Form />
    </>
    );
 }
